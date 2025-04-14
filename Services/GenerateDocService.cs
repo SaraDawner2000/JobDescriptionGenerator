aa1e94cb73278d8e8bea9e3c609e570690a7ec75
natadem2013 <83845798+SaraDawner2000@users.noreply.github.com>
2025-04-14 10:26:55 -0500

    clean lists of empty strings

--- Services/GenerateDocService.cs
+++ Services/GenerateDocService.cs
@@ -13,11 +13,20 @@ public class GenerateDocService : IGenerateDocService
 
     public byte[] GenerateDocument(JobDescriptionForm form)
     {
-        // TODO: Validate form (if needed)
+        // remove the empty or whitespace only strings
+        form.KeyAccountabilities = form.KeyAccountabilities
+        .Where(s => !string.IsNullOrWhiteSpace(s))
+        .ToList();
 
-        // Fetch JobTitle data from DB
+        form.KnowledgeAndSkills = form.KnowledgeAndSkills
+            .Where(s => !string.IsNullOrWhiteSpace(s))
+            .ToList();
+
         var jobTitle = _db.JobTitles
             .FirstOrDefault(t => t.JobTitleId == form.JobTitleId);
+
+        // EditForm ensures client-side validation, but if expanding to API or just for
+        // defence in depth and changes in the database, make sure to verify and throw errors
 
         if (jobTitle is null)
             throw new InvalidOperationException("Job title not found");
